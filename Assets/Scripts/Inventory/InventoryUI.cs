3891221e6dae429683cff1d74ee94c4e87ff9c7e
まぅSmallYuan <alan6716s@gmail.com>
2024-03-10 23:12:31 +0800

    0310-1112

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -84,32 +84,51 @@ public class InventoryUI : MonoBehaviour
     }
 
 
-    // 更新背包 UI 的方法
     public void UpdateInventoryUI()
     {
-        // 首先將所有的背包格子設為預設狀態，比如空的圖標
         foreach (var itemSlot in itemSlots)
         {
-            itemSlot.GetComponentInChildren<Image>().sprite = null  ; // 或是一個預設的空物品圖標
+            Image targetImage = null;
+            foreach (Transform child in itemSlot.transform)
+            {
+                if (child.CompareTag("ItemSlotImage"))
+                {
+                    targetImage = child.GetComponent<Image>();
+                    break;
+                }
+            }
+            
+            if (targetImage != null)
+            {
+                targetImage.sprite = null;
+            }
         }
 
-        // 根據玩家背包內的物品更新背包格子
-        for (int i = 0; i < player.inventory.Container.Count; i++)
+        
+        for (int i = 0; i < player.inventory.Container.Count && i < itemSlots.Length; i++)
         {
-            if (i < itemSlots.Length)
+            Image targetImage = null;
+            foreach (Transform child in itemSlots[i].transform)
             {
-                if (itemSlots[i] != null && itemSlots[i].GetComponentInChildren<Image>() != null && player.inventory.Container[i] != null && player.inventory.Container[i].item != null) 
+                if (child.CompareTag("ItemSlotImage"))
                 {
-                    itemSlots[i].GetComponentInChildren<Image>().sprite = player.inventory.Container[i].item.icon;
+                    targetImage = child.GetComponent<Image>();
+                    break;
                 }
-                else 
-                {
-                    Debug.LogError("在索引" + i + "檢測到空引用");
-                }
+            }
 
+            if (targetImage != null && player.inventory.Container[i] != null && player.inventory.Container[i].item != null)
+            {
+                targetImage.sprite = player.inventory.Container[i].item.icon;
+            }
+            else
+            {
+                Debug.LogError($"在索引 {i} 檢查到空引用");
             }
         }
     }
+
+
     void TryCraftItem()
     {
         bool crafted = false;
@@ -118,10 +137,8 @@ public class InventoryUI : MonoBehaviour
         {
             if (IsMatch(recipe.materials, selectedItems))
             {
-                // 配方匹配，進行合成
                 CraftItem(recipe);
                 crafted = true;
-                // 不再使用 break;，繼續遍歷所有配方
             }
         }
 
@@ -130,7 +147,6 @@ public class InventoryUI : MonoBehaviour
             Debug.Log("沒有找到合適的配方");
         }
 
-        // 只有在所有需要的物品都已選擇後才清空 selectedItems
         if (crafted)
         {
             selectedItems.Clear();
@@ -141,15 +157,14 @@ public class InventoryUI : MonoBehaviour
 
     bool IsMatch(List<ItemObject> requiredItems, List<ItemObject> selectedItems)
     {
-        // 檢查 selectedItems 是否包含所有 requiredItems
         foreach (var reqItem in requiredItems)
         {
             if (!selectedItems.Contains(reqItem))
             {
-                return false; // 如果缺少任何所需物品，返回 false
+                return false;
             }
         }
-        return true; // 所有所需物品都被選中
+        return true;
     }
 
 }
