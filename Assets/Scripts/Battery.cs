945ec7b26d223f911eb769dbd199f31981c9442b
まぅSmallYuan <alan6716s@gmail.com>
2024-03-21 00:54:08 +0800

    0321-0053
    現在點擊電池會導致玩家卡在新場景 目前未完成

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Microsoft.VisualBasic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Battery : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Battery : MonoBehaviour
     private Dialog dialog;
     private GameMasterScript gameMasterScript;
     private int progress = 0;
+    private bool isWaitingForDialog = false;
 
     void Awake()
     {
@@ -16,8 +18,28 @@ public class Battery : MonoBehaviour
         dialog = GameObject.Find("Dialog").GetComponent<Dialog>();
         gameMasterScript = FindObjectOfType<GameMasterScript>();
     }
+    
+
+    void Update()
+    {
+        if (!Dialog.DialogOn && !isWaitingForDialog)
+        {
+            switch (progress)
+            {
+                case 0:
+                    BatteryPickUp();
+                    break;
+                case 1:
+                    BatteryPickUp();
+                    break;
+            }
+        }
+    }
+
     public void BatteryPickUp()
     {
+        isWaitingForDialog = true;
+
         if (progress == 0)
         {
             dialog.GetDialog("FirstMonster", "BatteryPickUp");
@@ -25,9 +47,13 @@ public class Battery : MonoBehaviour
             {
                 progress = 1;
                 gameMasterScript.InventoryPanelDeactive();
+                isWaitingForDialog = false;
             });
         }
-        
-
+        else if (progress == 1)
+        {
+            SceneManager.LoadScene("QTE", LoadSceneMode.Additive);
+        }
     }
+
 }
