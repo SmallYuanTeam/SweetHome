221c3e1d3832994518f04369998f6696db18d634
Lovelymiku <104293530+Lovelymiku@users.noreply.github.com>
2024-03-21 00:42:43 +0800

    0321-0042

--- Assets/Scripts/StoryStart.cs
+++ Assets/Scripts/StoryStart.cs
@@ -10,32 +10,47 @@ public class StoryStart : MonoBehaviour
     private Dialog dialog;
     private GameMasterScript gameMasterScript;
     private bool hasDone = false;
+    private bool isCoroutineRunning = false;
     void Awake()
     {
         dialog = GameObject.Find("Dialog").GetComponent<Dialog>();
         sceneManagerHelper = FindObjectOfType<SceneManagerHelper>();
         gameMasterScript = FindObjectOfType<GameMasterScript>();
     }
+
     void Update()
     {
-        if (!Dialog.DialogOn && dialog.DialogIsCompleted() == true && !hasDone)
+        if (!Dialog.DialogOn && dialog.DialogIsCompleted() && !hasDone && !isCoroutineRunning)
         {
-            switch (progress)
+            StartCoroutine(ProcessStoryProgress());
+        }
+    }
+
+    IEnumerator ProcessStoryProgress()
+    {
+        isCoroutineRunning = true; // 标记协程正在运行
+
+        switch (progress)
             {
                 case 0:
-                    dialog.GetDialog("FirstStory", "FirstStory_01");
+                    dialog.GetDialog("FirstStory", "FirstStory_00");
                     dialog.setDialog();
                     progress = 1;
                     Dialog.DialogSkipOn = false;
                     break;
                 case 1:
-                    dialog.GetDialog("FirstStory", "FirstStory_00");
+                    yield return null;
+                    StartCoroutine(dialog.ChangeBackgroundForScripts("Mail", 0.3f, 0.3f));
+                    dialog.GetDialog("FirstStory", "FirstStory_01");
                     dialog.setDialog();
                     progress = 2;
                     Dialog.DialogSkipOn = false;
                     break;
                 case 2:
-                    dialog.ChangeBackgroundForScripts("GuysRoom", 0.3f, 0.3f);
+                    //yield return StartCoroutine(dialog.ChangeBackgroundForScripts("firstfloot_light", 2.0f, 2.0f));
+                    //yield return StartCoroutine(dialog.ChangeBackgroundForScripts("secondfloot_light", 3.0f, 4.0f));
+                    //yield return StartCoroutine(dialog.ChangeBackgroundForScripts("BookRoom_light", 5.0f, 6.0f));
+                    yield return null; 
                     dialog.GetDialog("FirstStory", "FirstStory_02");
                     dialog.setDialog();
                     progress = 3;
@@ -48,6 +63,8 @@ public class StoryStart : MonoBehaviour
                     hasDone = true;
                     break;
             }
-        }
+
+
+        isCoroutineRunning = false;
     }
 }
