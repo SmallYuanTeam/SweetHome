be2589769bd110d8919cfdcc4ded76956db2015e
まぅSmallYuan <alan6716s@gmail.com>
2024-03-23 20:04:42 +0800

    Update Item.cs

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -8,10 +8,13 @@ public class Item : MonoBehaviour
     public ItemObject item;
     public Player player;
     public Button button;
+    public string SceneName;
+    Dialog Dialog;
 
     void Start()
     {
         button = GetComponent<Button>();
+        Dialog = GameObject.Find("Dialog").GetComponent<Dialog>();
         // 找到玩家
         player = FindObjectOfType<Player>();
         if (Player.Instance.HasObtainedItem(item.itemID))
@@ -28,6 +31,9 @@ public class Item : MonoBehaviour
     {
         // 假设这个方法在物品被玩家拾取时被调用
         Player.Instance.AddItemToInventory(item);
+        // 從父對像身上得到場景
+        SceneName = transform.parent.name;
+        Dialog.GetRoomItem(SceneName, item.itemID);
         Player.Instance.AddObtainedItemID(item.itemID);
         gameObject.SetActive(false); // 隐藏物品
     }
